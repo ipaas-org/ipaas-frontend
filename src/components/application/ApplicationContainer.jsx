f90b16a7a9e6a1b69ab6e8779f545536f415005b
vano2903 <davidevanoncini2003@gmail.com>
2023-12-08 17:36:50 +0100

    style: styling

--- src/components/application/ApplicationContainer.jsx
+++ src/components/application/ApplicationContainer.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ApplicationItem from './ApplicationItem';
 import ApplicationModal from './ApplicationModal';
-import LoadingMessage from '../LoadingMessage';
+import {LoadingMessage} from '../LoadingMessage';
 import ErrorMessage from '../ErrorMessage';
 import useFetch from './../../utils/useFetch'
 
@@ -13,7 +13,7 @@ const ApplicationContainer = function () {
     <div className='custom-shadow overflow-hidden rounded-xl md:col-span-8'>
       <div className='h-full w-full divide-y divide-light-gray overflow-y-auto'>
         <div className='flex items-center justify-between px-8 py-6 text-xl font-semibold'>
-          <span>Application</span>
+          <span>Applications</span>
           <button onClick={() => setShowModal(true)} className='rounded bg-blue py-1 px-3 text-base text-white'>
             new
           </button>
--- src/components/database/DatabaseContainer.jsx
+++ src/components/database/DatabaseContainer.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react';
-import DatabaseItem from './DatabaseItem';
-import DatabaseModal from './DatabaseModal';
-import LoadingMessage from '../LoadingMessage';
-import ErrorMessage from '../ErrorMessage';
-import useFetch from './../../utils/useFetch'
-
-const sleep = function (s) {
-  return new Promise(res => setTimeout(res, s * 1000));
-};
+import {useState} from "react";
+import DatabaseItem from "./DatabaseItem";
+import DatabaseModal from "./DatabaseModal";
+import {LoadingMessage} from "../LoadingMessage";
+import ErrorMessage from "../ErrorMessage";
+import useFetch from "./../../utils/useFetch";
 
 const DatabaseContainer = function () {
-  const { data, loading, error } = useFetch();
+  const {data, loading, error} = useFetch();
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className='custom-shadow overflow-hidden rounded-xl md:col-span-4'>
-      <div className='h-full w-full divide-y divide-light-gray overflow-y-auto'>
-        <div className='flex items-center justify-between px-8 py-6 text-xl font-semibold'>
+    <div className="custom-shadow overflow-hidden rounded-xl md:col-span-4">
+      <div className="h-full w-full divide-y divide-light-gray overflow-y-auto">
+        <div className="flex items-center justify-between px-8 py-6 text-xl font-semibold">
           <span>Database</span>
-          <button onClick={() => setShowModal(true)} className='rounded bg-blue py-1 px-3 text-base text-white'>
+          <button
+            onClick={() => setShowModal(true)}
+            className="rounded bg-blue py-1 px-3 text-base text-white">
             new
           </button>
         </div>

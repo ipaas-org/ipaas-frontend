12c9c5b2727096571dfccaa5b8572cecf4aea866
vano2903 <davidevanoncini2003@gmail.com>
2023-12-08 17:30:50 +0100

    fix: remove error string

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,11 +1,5 @@
-const ErrorMessage = function ({ message }) {
-  return (
-    <div className='px-8 py-6'>
-      c'è stato un errore nel fetchare i contenuti:
-      <br />
-      {message.message}
-    </div>
-  );
+const ErrorMessage = function ({message}) {
+  return <div className="px-8 py-6">{message.message}</div>;
 };
 
 export default ErrorMessage;

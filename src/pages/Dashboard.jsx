c1ff452e89104a0639ec3f03606d4a8e00d2510d
vano2903 <davidevanoncini2003@gmail.com>
2024-03-28 19:09:47 +0100

    feat: add toaster and fix use memo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,18 @@
 import Header from "../components/header/Header";
 import ApplicationContainer from "../components/application/ApplicationContainer";
 import ServiceContainer from "../components/service/ServiceContainer";
-import {API} from "../utils/api";
-import {getAccessToken} from "../utils/tokens";
-import {useEffect, useMemo, useState} from "react";
+import { API } from "../utils/api";
+import { getAccessToken } from "../utils/tokens";
+import { ToastContainer, Zoom } from "react-toastify";
+import { useEffect, useMemo, useState } from "react";
 
-const Dashboard = function ({setLoggedIn}) {
+const Dashboard = function ({ setLoggedIn }) {
   const [applications, setApplications] = useState([]);
   const [services, setServices] = useState([]);
   const [servs, setServs] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [servicesFingerPrint, setServicesFingerPrint] = useState("");
 
   useMemo(() => {
     console.log("updating applications");
@@ -28,7 +30,7 @@ const Dashboard = function ({setLoggedIn}) {
     console.log("servs", s);
     setApplications(apps);
     setServices(s);
-  }, [servs]);
+  }, [servicesFingerPrint]);
 
   if (window.location.href.includes("key")) {
     window.location.href = "/dashboard";
@@ -44,12 +46,37 @@ const Dashboard = function ({setLoggedIn}) {
       },
     })
       .then((response) => {
-        console.log(response.data.data);
+        // console.log("all", response.data.data);
+        let fingerPrint = "";
+        if (!response.data.data) {
+          setServs(null);
+          setServicesFingerPrint("");
+          return;
+        }
+        response.data.data
+          .toSorted((a, b) => {
+            if (a.id < b.id) {
+              return -1;
+            }
+            if (a.id > b.id) {
+              return 1;
+            }
+            return 0;
+          })
+          .forEach((srv) => {
+            let envlen = 0;
+            if (srv.envs) {
+              envlen = srv.envs.length;
+            }
+            fingerPrint += `${srv.id}-${srv.state}-${srv.listeningPort}-${envlen}`;
+          });
+        // console.log(fingerPrint);
         setServs(response.data.data);
+        setServicesFingerPrint(fingerPrint);
       })
       .catch((error) => {
-        setError(error);
-        console.log(error);
+        console.error(error);
+        setError(error.response.details);
       });
     setLoading(false);
   };
@@ -67,6 +94,19 @@ const Dashboard = function ({setLoggedIn}) {
 
   return (
     <div className="relative h-screen px-2 md:px-10 lg:px-20">
+      <ToastContainer
+        position="bottom-right"
+        // autoClose={5000}
+        // hideProgressBar={false}
+        // newestOnTop={false}
+        // closeOnClick={true}
+        // rtl={false}
+        // pauseOnFocusLoss
+        // draggable
+        // pauseOnHover
+        // theme="light"
+        transition={Zoom}
+      />
       <Header setLoggedIn={setLoggedIn} />
       <main className="grid gap-5 md:h-5/6 md:grid-cols-12">
         <ApplicationContainer

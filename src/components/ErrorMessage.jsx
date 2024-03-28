f2cd082eaa2a4a1f2e6466a19559674788d27ea8
vano2903 <davidevanoncini2003@gmail.com>
2024-03-28 19:10:34 +0100

    feat: change style

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -2,8 +2,8 @@ const ErrorMessage = function ({message}) {
   return (
     <div
       role="alert"
-      class="relative flex w-full px-4 py-4 text-base bg-red-900/10 text-gray-900 border border-red-900 rounded-lg font-regular">
-      <div class="mr-12 ">
+      className="relative flex w-full px-4 py-4 text-base bg-red-900/10 text-gray-900 border border-red-900 rounded-lg font-regular">
+      <div className="mr-12 ">
         <span>{message}</span>
       </div>
     </div>
